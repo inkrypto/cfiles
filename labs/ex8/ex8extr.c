c2328c59b5d5035ca7828abbf0c1546b03bdf51f
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-03 15:39:15 +0000

    another example and clean up

--- labs/ex8/ex8extr.c
+++ labs/ex8/ex8extr.c
@@ -3,7 +3,8 @@
 int main(int argc, char *argv[])
 {
     int i = 0;
-    
+   
+    //not David's way but trying to use function instead 
     if(!isdigit(argv[i])){
         printf("Number\n");
     }     
---
+++ labs/ex8/ex8xtr1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    
+    if(argc == 1){
+        printf("You only have 1 arg. You suck!\n");
+    } else if (argc == 48)
+        printf("number\n");
+    return 0;
+}
