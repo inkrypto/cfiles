4918dc589a2cf0a6d514d020a67ad72015fe75ff
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-06 00:40:14 +0000

    xtr cred

--- labs/ex9/ex9extr.c
+++ labs/ex9/ex9extr.c
@@ -2,12 +2,22 @@
 
 int main(int argc, char *argv[])
 {
-    int i = 25;
-    while(i > 5 && i < 8){
-        printf("blast off in %d, seconds", i);
-        i--;
+    int i = 0;
+    while(i < 100){
+        if(i % 15 == 0){
+            printf("fizzbuzz");
+        }
+        else if(i % 5 == 0){
+            printf("fizz");
+        }
+        else if(i % 3 == 0){
+            printf("buzz");
+        }
+        else{
+           printf("%d",i);
+        }
+        i++;
         printf("\n");
     }
-    printf("\n");
     return 0;
 }
