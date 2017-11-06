bd33a11a0127a655bdafc951b241d84591b5c631
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-06 01:13:13 +0000

    string comparison no bueno

---
+++ labs/ex9/ex9xtr2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    \\trying to create a string
+    char secret="password";
+    \\trying to print a string
+    printf("The password is %s\n");
+    \\output = The password is e?N?  
+}
