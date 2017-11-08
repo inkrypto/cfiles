bea1afe36b8735d0aa5dc6b8882cfb8120e34032
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-08 15:02:54 +0000

    ex12

---
+++ labs/ex12/ex12.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    //go through each string in argv
+    //why am i skipping argv[0] -- because it's the name of the program
+    for(i=1; i<argc; i++){
+        printf("arg %d: %s\n", i, argv[i]);
+    }
+
+    //lets make our own array of strings
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Florida"
+    };
+
+    int num_states = 4;
+    
+    for(i=0; i < num_states; i++){
+        printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
