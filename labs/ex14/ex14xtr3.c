ee7999dc440fb2c2c63b7dea7c734fc4b330bdf0
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-14 15:03:49 +0000

    14

---
+++ labs/ex14/ex14xtr3.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<strings.h>
+
+//forward declarations
+int can_print_it(char ch);
+void print_letters(char arg[]);
+
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+
+    chr str[50];
+
+    int len;
+
+    len = print_letters;
+
+    for (i=0; i < argc; i++){
+       // strlen(print_letters(argv[i]));
+       (print_letters(argv[i]));
+        }
+    }
+
+void print_letters(char arg[])
+{       int i = 0;
+
+        for (i = 0; arg[i] != '\0'; i++){
+            char ch = arg[i];
+            
+            if (can_print_it(ch)){
+                printf("'%c' == %d ", ch, ch);
+            }
+        }
+    
+    printf("\n");
+}
+
+int can_print_it(char ch)
+{
+    return isalpha((int)ch) || isblank((int)ch);
+}
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+    return 0;
+}
