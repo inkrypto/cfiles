1578212af2a7aba01820a26aa103c7b3ff2443a9
Ubuntu <ubuntu@ip-172-31-34-201.us-west-2.compute.internal>
2017-11-17 19:36:33 +0000

    14

--- labs/ex14/ex14.c
+++ labs/ex14/ex14.c
@@ -1,39 +1,33 @@
 #include<stdio.h>
 #include<ctype.h>
 
-//forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+/*int can_print_it(char ch)
+{
+    return isalpha((int)ch) || isblank((int)ch);
+}*/
 
-void print_arguments(int argc, char **argv)
+void print_letters(char arg[])
+{       int i = 0;
+
+        for (i = 0; arg[i] != '\0'; i++){
+            //char ch = arg[i];
+            //if (ch){
+            printf("'%c' == %d\n", arg[i]);
+            }
+    printf("\n");
+}
+
+/*void print_arguments(int argc, char **argv)
 {
     int i = 0;
 
     for (i=0; i < argc; i++){
         print_letters(argv[i]);
         }
-    }
-
-void print_letters(char arg[])
-{       int i = 0;
-
-        for (i = 0; arg[i] != '\0'; i++){
-            char ch = arg[i];
-            if (can_print_it(ch)){
-                printf("'%c' == %d ", ch);
-            }
-        }
-    
-    printf("\n");
-}
-
-int can_print_it(char ch)
-{
-    return isalpha((int)ch) || isblank((int)ch);
-}
+    }*/
 
 int main(int argc, char *argv[])
 {
-    print_arguments(argc, argv);
+    print_letters(argc, argv);
     return 0;
 }
